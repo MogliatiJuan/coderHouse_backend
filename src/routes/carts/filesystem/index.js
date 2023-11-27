da3d31265e6c130f706e3e35927a6d638a7d61d9
Juan Manuel Mogliati <mogliatijm@gmail.com>
2023-11-27 16:37:04 -0300

    refactor: :zap: replace filesystem

---
+++ src/routes/carts/filesystem/index.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import { CartManager } from "../../controllers/index.js";
+
+const router = Router();
+const cartsRouter = new CartManager();
+
+router.post("/", async (request, response) => {
+  try {
+    const cart = await cartsRouter.createCart();
+    response
+      .status(201)
+      .send({ status: "success", message: "Cart created", carts: cart });
+  } catch (error) {
+    response.status(400).send({ status: "error", message: error.message });
+  }
+});
+
+router.get("/:cid", async (request, response) => {
+  try {
+    const cartId = await cartsRouter.getCartById(request.params.cid);
+    response.send({ status: "success", message: "Cart founded", cart: cartId });
+  } catch (error) {
+    response.status(404).send({ status: "error", message: error.message });
+  }
+});
+
+router.post("/:cid/product/:pid", async (request, response) => {
+  try {
+    const cartProduct = await cartsRouter.addProductToCart(
+      request.params.cid,
+      request.params.pid
+    );
+    response.send({
+      status: "success",
+      message: "Cart founded",
+      carts: cartProduct,
+    });
+  } catch (error) {
+    response.status(400).send({ status: "error", message: error.message });
+  }
+});
+
+export default router;
--- src/routes/views/index.js
+++ src/routes/views/index.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import { ProductManager } from "../../controllers/index.js";
+import { ProductManager } from "../../dao/index.js";
 
 const router = Router();
 const productManager = new ProductManager();
 
-router.get("/", async (request, response) => {
+router.get("/", async (_request, response) => {
   const products = await productManager.getProducts();
   response.render("home", { products });
 });
 
-router.get("/realtimeproducts", async (request, response) => {
+router.get("/realtimeproducts", async (_request, response) => {
   const products = await productManager.getProducts();
   response.render("realTimeProducts", { products });
 });
