569932d11cfccc8d3995ba4f01491768bfeb741a
Juan Manuel Mogliati <mogliatijm@gmail.com>
2023-11-27 16:34:36 -0300

    feat: :sparkles: install mongoose and dotenv, adapt app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import express from "express";
 import handlebars from "express-handlebars";
-import { Server } from "socket.io";
+import mongoose from "mongoose";
+//import { Server } from "socket.io";
+//import { ProductManager } from "./dao/filesystem/products/index.js";
 import __dirname from "./dirname.js";
 import router from "./routes/index.js";
-import { ProductManager } from "./controllers/index.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 8080;
-
-const productManager = new ProductManager();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,39 +23,59 @@ app.use(express.static(__dirname + "/public"));
 
 app.use("/api", router);
 
-const httpServer = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server PORT: ${PORT}`);
 });
 
-const socketServer = new Server(httpServer);
+//Database MongoDB
+const DB = `mongodb+srv://papu:${process.env.DB_PASSWORD}@ecommerce.6g4ke0l.mongodb.net/ecommerce?retryWrites=true&w=majority`;
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Conectado a MongoDB con Mongoose");
+  } catch (error) {
+    console.log("No se pudo conectar a la BD con Mongoose");
+    process.exit();
+  }
+};
 
-socketServer.on("connection", (socket) => {
-  console.log("Nuevo cliente conectado");
+connectMongoDB();
 
-  socket.on("products", async () => {
-    try {
-      const products = await productManager.getProducts();
-      socket.emit("products", products);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+//Socket
+//const productManager = new ProductManager();
+//const httpServer = app.listen(PORT, () => {
+//  console.log(`Server PORT: ${PORT}`);
+//});
 
-  socket.on("addProduct", async (product) => {
-    try {
-      const products = await productManager.addProduct(product);
-      socket.emit("products", products);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+//const socketServer = new Server(httpServer);
 
-  socket.on("deleteProduct", async (id) => {
-    try {
-      const products = await productManager.deleteProduct(id);
-      socket.emit("products", products);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-});
+//socketServer.on("connection", (socket) => {
+//  console.log("Nuevo cliente conectado");
+
+//  socket.on("products", async () => {
+//    try {
+//      const products = await productManager.getProducts();
+//      socket.emit("products", products);
+//    } catch (error) {
+//      console.log(error);
+//    }
+//  });
+
+//  socket.on("addProduct", async (product) => {
+//    try {
+//      const products = await productManager.addProduct(product);
+//      socket.emit("products", products);
+//    } catch (error) {
+//      console.log(error);
+//    }
+//  });
+
+//  socket.on("deleteProduct", async (id) => {
+//    try {
+//      const products = await productManager.deleteProduct(id);
+//      socket.emit("products", products);
+//    } catch (error) {
+//      console.log(error);
+//    }
+//  });
+//});
