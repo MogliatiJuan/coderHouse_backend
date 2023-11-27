848b6b1709a445dd4c6f5bc72b81df07969ec041
Juan Manuel Mogliati <mogliatijm@gmail.com>
2023-11-27 16:58:51 -0300

    feat: :sparkles: introduce chat

--- src/dao/models/index.js
+++ src/dao/models/index.js
@@ -1,2 +1,3 @@
 export { cartsMongo } from "./Carts/index.js";
 export { productsMongo } from "./Products/index.js";
+export { messageMongo } from "./Messages/index.js";
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import { Router } from "express";
 import cartRouter from "./carts/db/index.js";
 import productRouter from "./products/db/index.js";
 import viewRouter from "./views/index.js";
+import messageRouter from "./messages/index.js";
 
 const router = new Router();
 
 router.use("/products", productRouter);
 router.use("/carts", cartRouter);
 router.use("/views", viewRouter);
+router.use("/chat", messageRouter);
 
 export default router;
