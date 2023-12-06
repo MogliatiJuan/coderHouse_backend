a5b7cc7e81cb7761b4d9d3774df9b6260f036b7f
Juan Manuel Mogliati <mogliatijm@gmail.com>
2023-12-05 21:21:11 -0300

    fix: :bug: others changes...

--- src/app.js
+++ src/app.js
@@ -13,13 +13,14 @@ dotenv.config();
 const app = express();
 const PORT = 8080;
 
+app.set("port", process.env.PORT || 8080);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+handlebars.create({ allowProtoMethodsByDefault: true });
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
-
 app.use(express.static(__dirname + "/public"));
 
 app.use("/api", router);
