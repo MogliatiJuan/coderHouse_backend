a7d9613895cd1340d186be7a8795bcfb5b91962f
Juan Manuel Mogliati <mogliatijm@gmail.com>
2023-12-05 21:22:20 -0300

    feat: :zap: add products view

--- src/routes/views/index.js
+++ src/routes/views/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { ProductManager } from "../../dao/index.js";
+import { CartManagerMongo, ProductManager } from "../../dao/index.js";
+import { productsMongo } from "../../dao/models/index.js";
 
 const router = Router();
 const productManager = new ProductManager();
+const cartManager = new CartManagerMongo();
 
 router.get("/", async (_request, response) => {
   const products = await productManager.getProducts();
@@ -14,4 +16,26 @@ router.get("/realtimeproducts", async (_request, response) => {
   response.render("realTimeProducts", { products });
 });
 
+router.get("/products", async (req, res) => {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const products = await productsMongo.paginate(
+      {},
+      { page, limit, lean: true }
+    );
+    res.render("products", { products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching products");
+  }
+});
+
+router.get("/carts/:cid", async (req, res) => {
+  // probar :cid = 644d9bd2ec8f54b9724acf3c
+  try {
+    const { cid } = req.params;
+    let cart = await cartManager.getCartById(cid);
+    res.render("carts", { cart: cart.toJSON() });
+  } catch {}
+});
 export default router;
