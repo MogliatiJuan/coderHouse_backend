06cfd26dadeca1dbca47a637fecf6360de43b1b7
Juan Manuel Mogliati <mogliatijm@gmail.com>
2023-12-06 22:30:40 -0300

    feat: :sparkles: add new handlebars views and improve another

--- src/routes/views/index.js
+++ src/routes/views/index.js
@@ -18,12 +18,13 @@ router.get("/realtimeproducts", async (_request, response) => {
 
 router.get("/products", async (req, res) => {
   try {
+    if (!req.session.user) return res.redirect("/api/views/login");
     const { page = 1, limit = 10 } = req.query;
     const products = await productsMongo.paginate(
       {},
       { page, limit, lean: true }
     );
-    res.render("products", { products });
+    res.render("products", { products, user: req.session?.user });
   } catch (error) {
     console.error(error);
     res.status(500).send("Error fetching products");
@@ -33,9 +34,42 @@ router.get("/products", async (req, res) => {
 router.get("/carts/:cid", async (req, res) => {
   // probar :cid = 656fc3c70da0ff84e5f8001b
   try {
+    if (!req.session.user) return res.redirect("/api/views/login");
     const { cid } = req.params;
     let cart = await cartManager.getCartById(cid);
     res.render("carts", { cart: cart.toJSON() });
-  } catch {}
+  } catch {
+    console.log(error);
+    res.status(500).send({ message: error });
+  }
 });
 export default router;
+
+router.get("/profile", async (req, res) => {
+  try {
+    if (!req.session.user) return res.redirect("/api/views/login");
+    res.render("profile", { user: req.session.user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error });
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    if (req.session.user) return res.redirect("/api/views/products");
+    res.render("login", {});
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error });
+  }
+});
+router.get("/register", async (req, res) => {
+  try {
+    if (req.session.user) return res.redirect("/api/views/products");
+    res.render("register", {});
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error });
+  }
+});
