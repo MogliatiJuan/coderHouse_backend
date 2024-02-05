0644c159984de7d88211db3a88537335f9003268
Juan Manuel Mogliati <mogliatijm@gmail.com>
2024-02-05 07:59:21 -0300

    feat: :sparkles: introduce twilio

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-//import productRouter from "./products/index.js";
-//import cartRouter from "./carts/index.js";
-import cartRouter from "./carts/db/index.js";
-import productRouter from "./products/db/index.js";
+import cartRouter from "./carts.router.js";
+import productRouter from "./products.router.js";
 import viewRouter from "./views/index.js";
 import messageRouter from "./messages/index.js";
 import sessionsRouter from "./sessions/index.js";
+import notifications from "./api/notifications.js";
 
 const router = new Router();
 
@@ -14,5 +13,6 @@ router.use("/carts", cartRouter);
 router.use("/views", viewRouter);
 router.use("/chat", messageRouter);
 router.use("/sessions", sessionsRouter);
+router.use("/notifications", notifications);
 
 export default router;
